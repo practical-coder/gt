dc0d97426f257a80506f5bdce513d1f93ad7fec9
Jacek Witkiewicz <jacek.witkiewicz@gmail.com>
2022-08-10 10:41:32 +0200

    Git Repo: basic new/clone/pull/open functionality.

---
+++ git_repo.go
@@ -0,0 +1,192 @@
+package gt
+
+import (
+	"fmt"
+	"os"
+	"sync"
+
+	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	gitssh "github.com/go-git/go-git/v5/plumbing/transport/ssh"
+	"github.com/rs/zerolog"
+	ssh "golang.org/x/crypto/ssh"
+)
+
+var Logger zerolog.Logger
+var once sync.Once
+
+func init() {
+	once.Do(func() {
+		Logger = zerolog.New(zerolog.NewConsoleWriter()).With().Logger()
+	})
+}
+
+type GitRepo struct {
+	URL        string
+	Path       string
+	BranchName string
+	Repo       *git.Repository
+	privateKey []byte
+	PublicKeys *gitssh.PublicKeys
+}
+
+func (gr *GitRepo) SetKeys() error {
+	key, err := ssh.ParseRawPrivateKey(gr.privateKey)
+
+	if err != nil {
+		return fmt.Errorf("ParseRawPrivateKey Error: %v", err)
+	}
+
+	signer, err := ssh.NewSignerFromKey(key)
+	if err != nil {
+		return fmt.Errorf("NewSignerFromKey Error: %v", err)
+	}
+
+	publicKeys := gitssh.PublicKeys{
+		User:   "git",
+		Signer: signer,
+	}
+	gr.PublicKeys = &publicKeys
+
+	return nil
+}
+
+func New(repoURL, branchName, repoPath string, privateKey []byte) (*GitRepo, error) {
+	gitRepo := &GitRepo{
+		URL:        repoURL,
+		Path:       repoPath,
+		BranchName: branchName,
+		privateKey: privateKey,
+	}
+
+	err := gitRepo.SetKeys()
+	if err != nil {
+		return nil, err
+	}
+
+	return gitRepo, nil
+}
+
+func (gr *GitRepo) Open() {
+	repository, err := git.PlainOpen(gr.Path)
+	if err != nil {
+		Logger.Info().Err(err).Msg("Repo Open Error")
+	}
+	gr.Repo = repository
+}
+
+func (gr *GitRepo) LatestSHA(length int) string {
+	h, err := gr.Repo.ResolveRevision(plumbing.Revision(gr.BranchName))
+	if err != nil {
+		Logger.Info().Err(err).Msg("LatestSHA Error")
+	}
+	return h.String()[:length]
+}
+
+func (gr GitRepo) Worktree() *git.Worktree {
+	workTree, err := gr.Repo.Worktree()
+	if err != nil {
+		Logger.Info().Err(err).Msg("Repo Worktree Error")
+		return nil
+	}
+	return workTree
+}
+
+func (gr *GitRepo) Pull() error {
+	gr.Open()
+	if gr.Repo == nil {
+		return fmt.Errorf("Git Repository: Open Error. Path: %s", gr.Path)
+	}
+
+	w := gr.Worktree()
+	if w == nil {
+		return fmt.Errorf("Git Repository: Worktree Error. Path: %s", gr.Path)
+	}
+
+	err := w.Pull(gr.PullOptions())
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (gr *GitRepo) ReferenceName() plumbing.ReferenceName {
+	refName := fmt.Sprintf("refs/heads/%s", gr.BranchName)
+	return plumbing.ReferenceName(refName)
+}
+
+func (gr *GitRepo) CloneOptions() *git.CloneOptions {
+	cloneOptions := &git.CloneOptions{
+		Auth:          gr.PublicKeys,
+		URL:           gr.URL,
+		ReferenceName: gr.ReferenceName(),
+		Depth:         10,
+		Progress:      os.Stdout,
+	}
+
+	return cloneOptions
+}
+
+func (gr *GitRepo) PullOptions() *git.PullOptions {
+	pullOptions := &git.PullOptions{
+		Auth:          gr.PublicKeys,
+		RemoteName:    "origin",
+		ReferenceName: gr.ReferenceName(),
+	}
+
+	return pullOptions
+}
+
+func (gr *GitRepo) EnsurePath() error {
+	_, err := os.Stat(gr.Path)
+	if os.IsNotExist(err) {
+		err = os.Mkdir(gr.Path, 0755)
+		if err != nil {
+			cee.Logger.Info().
+				Err(err).
+				Str("repo_path", gr.Path).
+				Msg("Mkdir Error on repository dir path")
+		}
+	} else {
+		return err
+	}
+
+	return nil
+}
+
+func (gr *GitRepo) Clone() error {
+	err := gr.EnsurePath()
+	if err != nil {
+		return err
+	}
+
+	repository, err := git.PlainClone(gr.Path, false, gr.CloneOptions())
+
+	if err != nil {
+		return err
+	}
+
+	gr.Repo = repository
+
+	return nil
+}
+
+func (gr *GitRepo) CloneOrPull() error {
+	err := gr.Clone()
+
+	switch err {
+	case nil:
+		return nil
+	case git.ErrRepositoryAlreadyExists:
+		pullErr := gr.Pull()
+		if pullErr != nil {
+			return pullErr
+		}
+	default:
+		return err
+	}
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,30 @@
+module github.com/practical-coder/gt
+
+go 1.19
+
+require (
+	github.com/go-git/go-git/v5 v5.4.2
+	github.com/rs/zerolog v1.27.0
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa
+)
+
+require (
+	github.com/Microsoft/go-winio v0.5.2 // indirect
+	github.com/ProtonMail/go-crypto v0.0.0-20220810064516-de89276ce0f3 // indirect
+	github.com/acomagu/bufpipe v1.0.3 // indirect
+	github.com/cloudflare/circl v1.2.0 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
+	github.com/go-git/gcfg v1.5.0 // indirect
+	github.com/go-git/go-billy/v5 v5.3.1 // indirect
+	github.com/imdario/mergo v0.3.13 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/sergi/go-diff v1.2.0 // indirect
+	github.com/xanzy/ssh-agent v0.3.1 // indirect
+	golang.org/x/net v0.0.0-20220809184613-07c6da5e1ced // indirect
+	golang.org/x/sys v0.0.0-20220808155132-1c4a2a72c664 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
+)
